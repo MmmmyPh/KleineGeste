1d4261fe5702af778b4b560c56c8b9985d40eab5
俞展弘 <MmmmyPh@gmail.com>
2017-07-19 22:48:32 +0800

    config files

---
+++ webpack-config/dev.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+let pathToClean = [
+	'dist'
+];
+
+let cleanOption = {
+	verbose: false
+};
+
+module.exports = {
+	context: path.resolve(process.cwd(), 'src'),
+
+	entry: {
+		kleinegeste: './index.js'
+	},
+
+	output: {
+		filename: '[name].js',
+		path: path.resolve(process.cwd(), 'dist')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env']
+					}
+				}
+			}
+		]
+	},
+
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new CleanWebpackPlugin(pathToClean, cleanOption),
+		new HtmlWebpackPlugin({
+			filename: 'demo.html',
+			title: 'KleineGeste Demo',
+			template: path.resolve(process.cwd(), 'src/template.html')
+		})
+	],
+
+	devServer: {
+		hot: true,
+		contentBase: path.resolve(process.cwd(), 'dist'),
+		publicPath: '/'
+	}
+};
